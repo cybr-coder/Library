1293d98f0095378176242ca11c17f091519ba2ca
Lawrence Kelvin Nyarko <codercybr@gmail.com>
2025-06-30 04:55:49 +0200

    Completed JS object constructors exercise

---
+++ index.js
@@ -0,0 +1,14 @@
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    this.info = function() {
+        const readStatus = this.read ? "already read" : "not read yet";
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
+    };
+}
+
+const theHobbit = new Book ("The Hobbit", "J.R.R. Tolkien", 295, false);
+console.log(theHobbit.info());
