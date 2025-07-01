32c91bcf05613f7378a4e98856c39862e63fad58
Lawrence Kelvin Nyarko <codercybr@gmail.com>
2025-07-01 03:23:26 +0200

    Made three default book cards

--- index.js
+++ index.js
@@ -13,7 +13,34 @@ function Book(title, author, pages, read) {
     };
 }
 
-function addBookToLibrary(title, author, pages, read) {
-    const newBook = new Book(title, author, pages, read);
-    book.push(newBook);
+function addBookToLibrary(title, author, pages) {
+    const newBook = new Book(title, author, pages);
+    myLibrary.push(newBook);
 }
+
+function displayBooks() {
+    const bookList = document.getElementById('bookList');
+    bookList.innerHTML = '';
+
+    myLibrary.forEach(book => {
+        const card = document.createElement('div');
+        card.className = 'book-card';
+        card.innerHTML = `
+        <h3>${book.title}</h3>
+        <p><strong>Author:</strong> ${book.author}</p>
+        <p><strong>Published:</strong> ${book.pages}</p>
+        <p><small>ID: ${book.id}</small></p>
+        `;
+        bookList.appendChild(card);
+    });
+}
+
+
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 1960);
+addBookToLibrary("1984", "George Orwell", 1949);
+addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 1925);
+
+
+
+// Display them
+displayBooks();
